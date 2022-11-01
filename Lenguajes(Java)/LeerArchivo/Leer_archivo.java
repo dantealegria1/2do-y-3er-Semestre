7dc9c226eaefb3dffdadda081d840eed2059470b
dantealegria1 <101352597+dantealegria1@users.noreply.github.com>
2022-10-31 21:33:03 -0600

    Add files via upload

---
+++ Lenguajes(Java)/LeerArchivo/Leer_archivo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package leer_archivo;
+
+/**
+ *
+ * @author dante
+ */
+public class Leer_archivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Leer_archivo_ventana v= new Leer_archivo_ventana();
+        v.setVisible(true);
+    }
+    
+}
