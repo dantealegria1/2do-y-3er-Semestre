1ebd08bdaf13356a8dc6bd706d2f686ac1c8c3fc
dantealegria1 <101352597+dantealegria1@users.noreply.github.com>
2022-10-31 21:44:05 -0600

    Add files via upload

---
+++ Lenguajes(c#)/Ejercicio5_contraseña.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio4_contraseñas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Arriba el Atlas")
+            {
+                MessageBox.Show("Contraseña correcta");
+            }
+            else
+            {
+                MessageBox.Show("Contraseña incorrecta");
+            }
+            if (textBox1.Text != "Mar")
+            {
+                MessageBox.Show("Uusario incorrecte");
+            }
+        }
+    }
+}
---
+++ Lenguajes(c#)/ejercicio1_dias_de_la_semana.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interfaces_graficos_05_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e) 
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoy es Lunes ");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoy es Martes ");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoy es Miercoles ");
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoy es Jueves ");
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoy es Viernes ");
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoy es Sabado ");
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoy es Domingo ");
+        }
+    }
+}
