ac0cd5592ab339e00ffadb0fd374a10a42aa0d1f
dantealegria1 <101352597+dantealegria1@users.noreply.github.com>
2022-10-31 21:21:01 -0600

    Add files via upload

---
+++ Lenguajes(Java)/Examen2/Depresion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package new_examen_2;
+
+/**
+ *
+ * @author dante
+ */
+public class Depresion {
+    
+}
---
+++ Lenguajes(Java)/Examen2/New_Examen_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package new_examen_2;
+
+/**
+ *
+ * @author dante
+ */
+public class New_Examen_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
